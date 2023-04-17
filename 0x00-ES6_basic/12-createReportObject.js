201282d338ad7c1494ffc4171ddbc9837a811a17
rotex5 <dantejake48@gmail.com>
2023-04-17 19:30:52 +0100

    code refactore && elint style fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,9 +1,9 @@
 export default function createReportObject(employeesList) {
-
   return {
     allEmployees: { ...employeesList },
-    getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length;
+    getNumberOfDepartments: (employeesList) => {
+      const keyLen = Object.keys(employeesList).length;
+      return keyLen;
     },
   };
 }
