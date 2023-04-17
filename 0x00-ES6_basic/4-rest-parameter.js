c93b37ff3669fe7680b3d2d5ac551ecd783f01b9
rotex5 <dantejake48@gmail.com>
2023-04-17 17:13:25 +0100

    linting fix && code refactor

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,7 +1,3 @@
 export default function returnHowManyArguments(...args) {
-  let numOfArgs = 0;
-  for (let i = 0; i < args.length; i++) {
-    numOfArgs += 1;
-  };
-  return numOfArgs;
+  return args.length;
 }
