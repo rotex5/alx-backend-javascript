bd3e564e4888ffaaf0e5bc89fc83141e86666492
rotex5 <dantejake48@gmail.com>
2023-04-17 19:22:57 +0100

    create a report object fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -3,9 +3,8 @@ export default function createReportObject(employeesList) {
   return {
     allEmployees: { ...employeesList },
     getNumberOfDepartments: (employeesList) => {
-      const keys = Object.keys(employeesList);
-      // console.log(keys);
-      return keys.length;
+      const keyLen = Object.keys(employeesList).length;
+      return keyLen;
     },
   };
 }
