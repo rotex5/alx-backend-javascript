a501daead4c3451017eefeae88f079a83ad70b00
rotex5 <dantejake48@gmail.com>
2023-04-19 07:58:15 +0100

    implenting the use of finally clause

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,10 +1,8 @@
 function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      console.log('Got a response from the API');
-      return { status: 200, body: 'success' };
-    })
-    .catch(() => new Error());
+    .then(() => ({ status: 200, body: 'success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
 
 export default handleResponseFromAPI;
