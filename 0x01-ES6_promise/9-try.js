721869c7b1460c20026bf63070374ff221fb1001
rotex5 <dantejake48@gmail.com>
2023-04-19 19:18:56 +0100

    Expected Error message fix

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -3,7 +3,7 @@ function guardrail(mathFunction) {
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
     queue.push('Guardrail was processed');
   }
