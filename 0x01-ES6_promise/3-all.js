ce696a1c011c33cbf1bce2cfc347d9fbcabe718e
rotex5 <dantejake48@gmail.com>
2023-04-19 08:23:26 +0100

    handling multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,13 @@
+import { createUser, uploadPhoto } from './utils';
+
+function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then((values) => {
+      const firstName = values[0];
+      const lastName = values[0];
+      const body = values[1];
+      console.log(`${body.body} ${firstName.firstName} ${lastName.lastName}`);
+    });
+}
+
+export default handleProfileSignup;
