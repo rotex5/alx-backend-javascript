c3496e1ce73251206226c3dee7bfa7d9e7728892
rotex5 <dantejake48@gmail.com>
2023-04-21 00:26:34 +0100

    implementing class methods, setters and getters

---
+++ 0x02-ES6_classes/3-currency.js
@@ -0,0 +1,30 @@
+class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  // getters
+  get code() {
+    return this._code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  // setters
+  set code(value) {
+    this._code = value;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
+
+export default Currency;
