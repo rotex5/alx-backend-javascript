1ca57878f817c9168d2285dd81150bf1b12aea08
rotex5 <dantejake48@gmail.com>
2023-04-21 01:56:26 +0100

    task 5 code fix-Check to ensure class that extends has an overiding function, task 6- inheritaning abstart class

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.constructor !== Building && this.evacuationWarningMessage !== 'function') {
+    if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
   }
