a90a58bdcec7c9c2bee6a7c5b17e7e3cc84d2605
rotex <dantejake48@gmail.com>
2023-04-25 18:42:01 +0100

    returns a new ArrayBuffer with an Int8 value at a specific position

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,20 @@
+function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) throw new Error('Position outside range');
+
+  // setting length of new ArrayBuffer
+  const buffer = new ArrayBuffer(length);
+
+  // setting Int8Array view on buffer
+  // const int8Array = new Int8Array(buffer);
+
+  // int8Array[position] = value;
+
+  // second method
+  const dv1 = new DataView(buffer);
+
+  dv1.setInt8(position, value);
+
+  return buffer;
+}
+
+export default createInt8TypedArray;
