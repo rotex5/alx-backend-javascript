7bde2aac96051e705234e83a634d1d3e6222a756
rotex <dantejake48@gmail.com>
2023-04-25 23:05:29 +0100

    code refactor

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,13 +1,22 @@
 function cleanSet(set, startString) {
   if (startString === '' || startString === undefined) return '';
 
+  const result = [];
   const newArray = new Array(...set);
-  const result = newArray
+  /*
+   * const result = newArray
     .filter((elem) => (elem !== undefined ? elem.startsWith(startString) : ''))
     .map((value) => (value !== undefined ? value.slice(startString.length) : ''))
     .join('-');
+    */
 
-  return result;
+  for (const item of newArray) {
+    if (item && item.startsWith(startString)) {
+      result.push(item !== undefined ? item.slice(startString.length) : '');
+    }
+  }
+
+  return result.join('-');
 }
 
 export default cleanSet;
