4dc98a7f61df150415910be7266951201d503034
rotex <dantejake48@gmail.com>
2023-04-25 19:28:29 +0100

    returns a boolean if all the elements in the array exist within the set.

---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,20 @@
+function hasValuesFromArray(setParams, arrayParams) {
+  // const subSet = new Set(arrayParams);
+  // return setParams.has(subSet);
+  /*
+    for (let i = 0; i < arrayParams.length; i++) {
+    if (!setParams.has(arrayParams[i])) {
+      return false;
+    }
+  }
+  return true;
+  */
+  // issue with i++
+
+  for (const elem of arrayParams) {
+    if (!setParams.has(elem)) return false;
+  }
+  return true;
+}
+
+export default hasValuesFromArray;
