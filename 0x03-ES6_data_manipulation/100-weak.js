8f4c17e3caa7d48f1f921b89dc227dcbaa368862
rotex <dantejake48@gmail.com>
2023-04-26 00:12:21 +0100

    tracking api calls

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,14 @@
+const weakMap = new WeakMap();
+
+function queryAPI(endpoint) {
+  let counter = weakMap.get(endpoint) || 0;
+  counter += 1;
+
+  if (counter >= 5) {
+    throw new Error('Endpoint load is high');
+  }
+
+  weakMap.set(endpoint, counter);
+}
+
+export default { weakMap, queryAPI };
