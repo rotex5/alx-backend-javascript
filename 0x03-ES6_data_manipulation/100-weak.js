f84a5653dbe4c85d6dbf823fc4b3e760cd559505
rotex <dantejake48@gmail.com>
2023-04-26 01:17:27 +0100

    tracking api calls

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,14 +1,22 @@
-const weakMap = new WeakMap();
+export const weakMap = new WeakMap();
 
-function queryAPI(endpoint) {
-  let counter = weakMap.get(endpoint) || 0;
-  counter += 1;
+export function queryAPI(endpoint) {
+  let count = 0;
 
-  if (counter >= 5) {
+  count = weakMap.get(endpoint) || 0;
+  /*
+   * We then use the get method of the WeakMap to retrieve
+   * the current count for this endpoint. If the count does
+   * not exist yet (i.e., the endpoint has not been queried
+   * before), we set the count to 0. We increment the count
+   * by 1 and check if it's greater than or equal to 5. If it
+   * is, we throw an error with the message 'Endpoint load is
+   * high'. If not, we update the count for this endpoint in
+   * the WeakMap using the set method.
+  */
+  if (count >= 5) {
     throw new Error('Endpoint load is high');
   }
 
-  weakMap.set(endpoint, counter);
+  weakMap.set(endpoint, count += 1);
 }
-
-export default { weakMap, queryAPI };
