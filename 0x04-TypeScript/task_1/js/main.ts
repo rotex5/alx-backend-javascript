7e122b84fe47a0261c912130638667b6c63a94be
rotex <dantejake48@gmail.com>
2023-04-26 23:26:49 +0100

    Created a director interface that extends from Teacher interface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -7,3 +7,29 @@ interface Teacher {
   location: string;
   [propName: string]: any;
 }
+
+/*
+const teacher3: Teacher = {
+  firstName: 'John',
+  fullTimeEmployee: false,
+  lastName: 'Doe',
+  location: 'London',
+  contract: false,
+};
+console.log(teacher3);
+*/
+
+interface Directors extends Teacher {
+  numberOfReports: number,
+}
+
+/*
+const director1: Directors = {
+  firstName: 'John',
+  lastName: 'Doe',
+  location: 'London',
+  fullTimeEmployee: true,
+  numberOfReports: 17,
+};
+console.log(director1);
+*/
