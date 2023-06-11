c101cacc7b53269d6becd599095ab9fb903dc375
rotex <dantejake48@gmail.com>
2023-06-11 16:02:29 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,5 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.setEncoding('utf8');
 
 process.stdin.on('readable', function() {
   const name = process.stdin.read();
