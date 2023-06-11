deba3d30b65ffb9c5968592aeeab0f5d71a6ad10
rotex <dantejake48@gmail.com>
2023-06-11 15:41:16 +0100

    Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,9 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', function() {
-
   const name = process.stdin.read();
-
   if (name !== null) {
     process.stdout.write(`Your name is: ${name}`);
   }
